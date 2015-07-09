d1b2ff133bce104b12e3e9136bb04631ceb0ea92
Marpriori <marcimpriori@gmail.com>
2015-07-09 18:25:36 -0300

    TCCC
    Acessando e gerando json dos horários do site da TCCC.

---
+++ TcccAccess/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace TcccAccess
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 12);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(448, 284);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri("http://www.tccc.com.br/horario.aspx", System.UriKind.Absolute);
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.WebBrowser1DocumentCompleted);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 302);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(448, 386);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(466, 93);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(613, 303);
+            this.listBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(466, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "IR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(466, 409);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(212, 277);
+            this.listBox2.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(547, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(532, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // listBox3
+            // 
+            this.listBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(684, 409);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(177, 277);
+            this.listBox3.TabIndex = 6;
+            // 
+            // listBox4
+            // 
+            this.listBox4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(867, 409);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(212, 277);
+            this.listBox4.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1091, 700);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox4;
+    }
+}
+
---
+++ TcccAccess/Form1.cs
@@ -0,0 +1,432 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.WebControls;
+using System.Windows.Forms;
+using TcccAccess.Types;
+
+namespace TcccAccess
+{
+    public partial class Form1 : Form
+    {
+
+        List<Types.Rota> listaRotas = new List<Types.Rota>();
+        private Types.Rota rotaAtual = null;
+        private int origemAtual = 0;
+        private int diaAtual = 0; //Vai até 2
+        private int situacao = 0;
+
+        private const int SELECT_ROTA = 1;
+        private const int SELECT_ORIGEM = 2;
+        private const int SELECT_DIA = 3;
+        private const int CLICK_BOTAO = 4;
+        private const int SALVAR_DADOS = 5;
+        private const int ACABOU = 6;
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            webBrowser1 = new WebBrowser();
+            webBrowser1.Navigate("http://www.tccc.com.br");
+            
+        }
+
+        private void WebBrowser1DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            listBox1.Items.Add(webBrowser1.Url);
+            Refresh();
+            RodarProcesso();
+
+        }
+
+        private void RodarProcesso()
+        {
+          bool eventoDid = false;
+            if (listaRotas.Count == 0) CarregarListaRota();
+
+            switch (situacao)
+            {
+                case SELECT_ROTA:
+                   eventoDid = SelecionarRota();
+                    break;
+                case SELECT_ORIGEM:
+                    eventoDid = SelecionarOrigem();
+                    break;
+                case SELECT_DIA:
+                    SelecionarDia();
+                    break;
+                case CLICK_BOTAO:
+                    ClickBotao();
+                    eventoDid = true;
+                    break;
+                case SALVAR_DADOS:
+                    SalvarDados();
+                    break;
+            }
+
+            if(!eventoDid)
+                RodarProcesso();
+        }
+
+        private void SalvarDados()
+        {
+            if (webBrowser1.Document.GetElementById("ctl00_ContentPlaceHolder3_lblNomeLinha") == null)
+            {
+                ClickBotao();
+                return;
+            }
+            string nomeSentido = webBrowser1.Document.GetElementById("ctl00_ContentPlaceHolder3_lblNomeDestino").InnerHtml;
+            
+            Sentido sentido=null;
+            
+            foreach (Sentido sentido1 in rotaAtual.Sentidos.Where(sentido1 => sentido1.Nome == nomeSentido))
+            {
+                sentido = sentido1;
+            }
+            if(sentido == null)
+            {
+                sentido = new Sentido();
+                rotaAtual.Sentidos.Add(sentido);    
+
+            }
+            sentido.Nome = nomeSentido;
+            Dia dia = null;
+            string nomeDia = webBrowser1.Document.GetElementById("ctl00_ContentPlaceHolder3_lblDia").InnerHtml;
+            foreach ( Dia d in sentido.Dias.Where(d2=>d2.Nome == nomeDia))
+            {
+                dia = d;
+            }
+            if(dia == null)
+            {
+                dia = new Dia();
+                sentido.Dias.Add(dia);
+            }
+            
+            dia.Nome = nomeDia;
+
+            string valorBase = string.Format("");
+            listBox2.Items.Add(webBrowser1.Document.GetElementById(
+                                               "ctl00_ContentPlaceHolder3_lblNomeLinha").InnerHtml);
+            listBox3.Items.Add(webBrowser1.Document.GetElementById(
+                "ctl00_ContentPlaceHolder3_lblNomeDestino").InnerHtml);
+            listBox4.Items.Add(webBrowser1.Document.GetElementById("ctl00_ContentPlaceHolder3_lblDia").
+                                   InnerHtml);
+            listBox2.SelectedIndex = listBox2.Items.Count - 1;
+            listBox3.SelectedIndex = listBox3.Items.Count - 1;
+            listBox4.SelectedIndex = listBox4.Items.Count - 1;
+
+
+            if (webBrowser1.Document.GetElementById("ctl00_ContentPlaceHolder3_GridView1") != null)
+            {
+                int x = 0;
+
+                string valor = "";
+
+                Horario horario = new Horario();
+                foreach (
+                    HtmlElement VARIABLE in
+                        webBrowser1.Document.GetElementById("ctl00_ContentPlaceHolder3_GridView1").
+                            GetElementsByTagName("td"))
+                {
+
+                    switch (x)
+                    {
+                        case 0: //partida
+                            valor += VARIABLE.InnerHtml;
+                            horario.Partida = VARIABLE.InnerHtml;
+                            break;
+                        case 1: //chegada
+                            valor += " - " + VARIABLE.InnerHtml;
+                            horario.Chegada = VARIABLE.InnerHtml;
+                            break;
+                        case 2: // obs
+                            if (VARIABLE.InnerHtml != "&nbsp;")
+                            {
+                                valor += " - " + VARIABLE.InnerHtml;
+                                horario.Observacao = VARIABLE.InnerHtml;
+                            }
+
+                            listBox1.Items.Add(valor);
+                            valor = valorBase;
+                            break;
+                    }
+                    if (x == 2)
+                    {
+                        dia.Horarios.Add(new Horario()
+                                                 {
+                                                     Chegada = horario.Chegada,
+                                                     Partida = horario.Partida,
+                                                     Observacao = horario.Observacao
+                                                 });
+                        horario = new Horario();
+                        x = 0;
+                    }
+                    else x++;
+                }
+
+            }
+
+            if (webBrowser1.Document.GetElementById("ctl00_ContentPlaceHolder3_GridView2") != null)
+            {
+                listBox2.Items.Add(valorBase);
+                if (sentido.Etinerario.Count == 0)
+                {
+                    foreach (
+                        HtmlElement VARIABLE in
+                            webBrowser1.Document.GetElementById("ctl00_ContentPlaceHolder3_GridView2").
+                                GetElementsByTagName("td"))
+                    {
+                        sentido.Etinerario.Add(VARIABLE.InnerHtml);
+                        listBox2.Items.Add(VARIABLE.InnerHtml);
+                    }
+                }
+            }
+            if (origemAtual < rotaAtual.Origens.Count)
+            {
+                origemAtual++;
+                situacao = SELECT_ORIGEM;
+            }
+            else
+            {
+                origemAtual = 0;
+                JSONSerialize(rotaAtual);
+                listBox1.Refresh();
+                listBox2.Refresh();
+                situacao = SELECT_ROTA;
+            }
+            return;
+
+        }
+
+        private void JSONSerialize(Rota rotaAtual)
+        {
+            MemoryStream stream = new MemoryStream();
+            DataContractJsonSerializer jsonSer = new DataContractJsonSerializer(typeof(Rota));
+            jsonSer.WriteObject(stream, rotaAtual);
+            stream.Position = 0;
+            StreamReader sr = new StreamReader(stream);
+            File.WriteAllText(string.Format("D:/rota/{1} - {0}.json", rotaAtual.Nome, DateTime.Now.ToString("yyyy_MM_dd_HHmmss")), sr.ReadToEnd(), Encoding.UTF8);
+        }
+
+        private void ClickBotao()
+        {
+            HtmlElement botalEl = webBrowser1.Document.GetElementById("UChorarios1_btnConsultar");
+            if (botalEl == null)
+                botalEl = webBrowser1.Document.GetElementById("ctl00_ContentPlaceHolder2_UChorarios1_btnConsultar");
+            situacao = SALVAR_DADOS;
+            botalEl.InvokeMember("click");
+        }
+
+        private void SelecionarDia()
+        {
+            HtmlElement diaEl = webBrowser1.Document.GetElementById("UChorarios1_dropTipo");
+            if(diaEl == null) diaEl = webBrowser1.Document.GetElementById("ctl00_ContentPlaceHolder2_UChorarios1_dropTipo");
+
+            if (diaAtual > 2)
+                diaAtual = 0;
+
+            if(diaEl.GetAttribute("SelectedIndex") != diaAtual.ToString())
+            {
+                diaEl.SetAttribute("SelectedIndex", diaAtual.ToString());
+            }
+            situacao = CLICK_BOTAO;
+           // RodarProcesso();
+
+        }
+
+        private bool SelecionarOrigem()
+        {
+            bool eventoDid = false;
+            #region Carregar Origens
+
+            HtmlElement origemEl = webBrowser1.Document.GetElementById("UChorarios1_dropOrigem");
+            if (origemEl == null)
+                origemEl = webBrowser1.Document.GetElementById("ctl00_ContentPlaceHolder2_UChorarios1_dropOrigem");
+            if (rotaAtual.Origens.Count == 0)
+            {
+                if (origemEl != null)
+                {
+                    foreach (HtmlElement el in origemEl.GetElementsByTagName("option"))
+                    {
+                       // listBox1.Items.Add(el.InnerHtml);
+                        rotaAtual.Origens.Add(new ListItem(el.InnerText, el.GetAttribute("value")));
+                    }
+                }
+            }
+            if (origemEl != null)
+            {
+                string selected = origemEl.GetAttribute("selectedIndex");
+                if (selected != origemAtual.ToString())
+                {
+                    origemEl.SetAttribute("SelectedIndex", origemAtual.ToString());
+                    
+                    
+                    eventoDid = true;
+                    origemEl.RaiseEvent("onChange");
+                }
+                //origemAtual++;
+                situacao = SELECT_DIA;
+            }
+
+            #endregion Carregar Origens
+
+            return eventoDid;
+            // situacao = SELECT_DIA;
+            //RodarProcesso();
+        }
+
+        private bool SelecionarRota()
+        {
+            bool eventoDid = false;
+            //CARREGAR ROTA
+            #region CarregarRota
+            bool proxima = false;
+            if (rotaAtual == null)
+            {
+                rotaAtual = listaRotas.First();
+            }
+            else
+            {
+                if (rotaAtual.Codigo == listaRotas.Last().Codigo && diaAtual == 2)
+                {
+
+                    situacao = ACABOU;
+                    return true;
+
+
+                }
+                else
+                {
+                    if (rotaAtual.Codigo == listaRotas.Last().Codigo)
+                    {
+                        diaAtual++;
+                        rotaAtual = listaRotas.First();
+                    }
+                    else
+                    {
+                        foreach (Types.Rota rota in listaRotas)
+                        {
+                            if (proxima)
+                            {
+                                rotaAtual = rota;
+                                break;
+                            }
+                            if (rota.Codigo == rotaAtual.Codigo) proxima = true;
+                        }
+                    }
+                }
+            }
+            comboBox1.SelectedIndex = comboBox1.FindString(rotaAtual.Codigo);
+            #endregion CarregarRota
+
+            #region SET Rota WEb
+            HtmlElement rotaEl = webBrowser1.Document.GetElementById("UChorarios1_droLinha");
+            if (rotaEl == null) rotaEl = webBrowser1.Document.GetElementById("ctl00_ContentPlaceHolder2_UChorarios1_droLinha");
+            if(rotaEl != null)
+            {
+                int i = 0;
+                foreach (HtmlElement element in rotaEl.GetElementsByTagName("option"))
+                {
+                    if (element.GetAttribute("value") == rotaAtual.Codigo) break;
+                    i++;
+                }
+                situacao = SELECT_ORIGEM;
+                rotaEl.SetAttribute("SelectedIndex", i.ToString());
+                eventoDid = true;
+                rotaEl.RaiseEvent("onChange");
+            }
+            #endregion
+
+            return eventoDid;
+        }
+
+        private void CarregarListaRota()
+        {
+            if (webBrowser1.Document != null)
+            {
+                HtmlElement el = webBrowser1.Document.GetElementById("UChorarios1_droLinha");
+                if (el != null)
+                {
+                    foreach (HtmlElement element in el.GetElementsByTagName("option"))
+                    {
+                        string value = element.GetAttribute("value");
+                        if (string.IsNullOrEmpty(value) || value == "SELECIONE" || value == "0") continue;
+
+                        listaRotas.Add(new Types.Rota() { Nome = element.InnerText, Codigo = value });
+                    }
+                }
+            }
+
+            comboBox1.DataSource = listaRotas;
+            comboBox1.DisplayMember = "Nome";
+            comboBox1.ValueMember = "Codigo";
+            comboBox1.Refresh();
+
+            situacao = SELECT_ROTA;
+        }
+    }
+
+    namespace Types
+    {
+    public class Rota
+    {
+        public Rota()
+        {
+            Origens = new List<ListItem>();
+            Sentidos = new List<Sentido>();
+        }
+        public string Codigo { get; set; }
+        public string Nome { get; set; }
+
+        public List<ListItem> Origens { get; set; }
+        public List<Sentido> Sentidos { get; set; } 
+    }
+
+        public class Sentido
+        {
+            public Sentido()
+            {
+                Dias = new List<Dia>();
+               
+                Etinerario = new List<string>();
+            }
+            public List<string> Etinerario { get; set; } 
+            public string Nome { get; set; }
+            public List<Dia> Dias { get; set; } 
+            
+            
+        }
+        public class Dia
+        {
+            public Dia()
+            {
+                Horarios = new List<Horario>(); 
+         
+            }
+            public string Nome { get; set; }
+            public List<Horario> Horarios { get; set; }
+           
+        }
+        public class Horario
+        {
+            public string Partida { get; set; }
+            public string Chegada { get; set; }
+            public string Observacao { get; set; }
+        }
+
+
+}
+}
